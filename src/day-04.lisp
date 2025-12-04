5cb35c1c0ff4ec853300681749402c143cd522c9
David Clark <me@nobody.com>
2025-12-03 23:41:47 -0600

    Refactored day 04

--- src/day-04.lisp
+++ src/day-04.lisp
@@ -1,7 +1,6 @@
 (defpackage :day-04
   (:use #:cl)
   (:import-from :utils #:read-day-file #:*to-add/8*)
-  (:import-from :alexandria #:rcurry)
   (:export #:part-1 #:part-2))
 
 (in-package :day-04)
@@ -17,35 +16,32 @@
 			(setf (gethash (cons row col) table) t)))
 	finally (return table)))
 
-(defun part-1 ()
-  (flet ((cell+ (one two) (cons (+ (car one) (car two)) (+ (cdr one) (cdr two)))))
-    (let ((table (get-grid "04"))
-	  (total 0))
-      (loop for cell being the hash-keys in table
-	    do (if (< (reduce #'+ (mapcar #'(lambda (to-add)
-					      (let ((new-cell (cell+ to-add cell)))
-						(if (gethash new-cell table) 1 0))) *to-add/8*)) 4)
-		   (incf total))
-	    finally (return total)))))
-
-(defun can-remove (table)
+(defun iterate-removable (table func)
   (flet ((cell+ (one two) (cons (+ (car one) (car two)) (+ (cdr one) (cdr two)))))
     (loop with ret = nil
 	  for cell being the hash-keys in table
 	  do (if (< (reduce #'+ (mapcar #'(lambda (to-add)
 					    (let ((new-cell (cell+ to-add cell)))
 					      (if (gethash new-cell table) 1 0))) *to-add/8*)) 4)
-		 (push cell ret))
-	  finally (return ret))))
+		 (funcall func cell)))))
+
+(defun part-1 ()
+  (let ((table (get-grid "04"))
+	(total 0))
+    (iterate-removable table #'(lambda (cell) (incf total)))
+    total))
 
 (defun part-2 ()
   (let ((table (get-grid "04"))
-	(removed 0))
-    (loop for to-remove = (can-remove table) then (can-remove table)
-	  while to-remove
-	  do (incf removed (length to-remove))
-	     (dolist (cell to-remove)
-	       (remhash cell table)))
-    removed))
-	
+	(removed 0)
+	(to-remove nil))
+    (flet ((add (cell) (push cell to-remove)))
+      (loop do (iterate-removable table #'add)
+	       (if to-remove
+		   (progn
+		     (incf removed (length to-remove))
+		     (dolist (cell to-remove)
+		       (remhash cell table))
+		     (setf to-remove nil))
+		   (return removed))))))
 
