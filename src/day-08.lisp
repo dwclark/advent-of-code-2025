43f80931fd1bb23f604b7d7553e57bd7a36c89ae
David Clark <me@nobody.com>
2025-12-09 00:43:50 -0600

    Finished part 2 of day 8

--- src/day-08.lisp
+++ src/day-08.lisp
@@ -19,83 +19,95 @@
 	   (expt (- p1 p2) 2)))
     (sqrt (reduce #'+ (mapcar #'part l1 l2)))))
 
-(defun total-connections (data)
-  (loop for i from (length data) downto 1
-	summing (- i 1)))
+(defstruct metric
+  (one nil :type cons)
+  (two nil :type cons)
+  (distance 0d0 :type float))
 
-(defun make-distances (data)
-  (loop with distances = (make-hash-table)
-	for l1 on data
-	do (if (rest l1)
-	       (loop with one = (first l1)
-		     for two in (rest l1)
-		     do (setf (gethash (distance one two) distances) (cons one two))))
-	finally (return (values (sort (hash-table-keys distances) #'<) distances))))
+(defun new-metric (one two)
+  (make-metric :one one :two two :distance (distance one two)))
 
-(defun make-circuits (distances table)
-  (let ((circuits nil))
-    (labels ((add (one two)
-	       (let ((found (find-if (lambda (circuit)
-				       (or (gethash one circuit)
-					   (gethash two circuit))) circuits)))
-		 (if found
-		     (progn
-		       (setf (gethash one found) t
-			     (gethash two found) t)
-		       found)
-		     (let ((new-circuit (make-hash-table :test 'equal)))
-		       (setf (gethash one new-circuit) t
-			     (gethash two new-circuit) t)
-		       (push new-circuit circuits)
-		       new-circuit))))
+(defun make-metrics (data)
+  (loop with ret = nil
+	for all on data
+	do (if (rest all)
+	       (loop with one = (first all)
+		     for two in (rest all)
+		     do (push (new-metric one two) ret)))
+	finally (return (sort ret #'< :key #'metric-distance))))
 
-	     (overlaps-p (m1 m2)
-	       (when (not (eq m1 m2))
-		 (loop for key being the hash-keys in m1
-		       do (if (gethash key m2)
-			      (return t))
-		       finally (return nil))))
-	     
-	     (join-circuits (circuit)
-	       (let ((overlap (find-if (curry #'overlaps-p circuit) circuits)))
-		 (when overlap
-		   (loop for key being the hash-keys in circuit
-			 do (setf (gethash key overlap) t))
-		   (setf circuits (remove-if (curry #'eq circuit) circuits))
-		   (join-circuits overlap)))))
+(defun overlap-p (c1 c2)
+  (if (eq c1 c2)
+      nil
+      (loop for junction being the hash-keys in c1
+	    do (if (gethash junction c2)
+		   (return t))
+	    finally (return nil))))
 
-      (dolist (distance distances)
-	(destructuring-bind (one . two) (gethash distance table)
-	  (let ((circuit (add one two)))
-	    (join-circuits circuit))))
+(defun how-p (metric circuits)
+  (loop with one = (metric-one metric)
+	with two = (metric-two metric)
+	for circuit in circuits
+	do (let ((one-in (gethash one circuit))
+		 (two-in (gethash two circuit)))
+	     (cond ((and one-in two-in)
+		    (return (values :noaction circuit)))
+		   ((or one-in two-in)
+		    (return (values :join circuit)))))
+	finally (return (values :new nil))))
 
-      (sort circuits #'> :key #'hash-table-count))))
+(defun merge-circuits (new circuits)
+  (let ((merge-to (find-if (curry #'overlap-p new) circuits)))
+    (if merge-to
+      (loop for junction being the hash-keys in new
+	    do (setf (gethash junction merge-to) t)
+	    finally (return (merge-circuits merge-to (remove-if (curry #'eq new) circuits))))
+      circuits)))
+
+(defun remove-already-in (metrics circuits)
+  (flet ((already (metric)
+	   (loop for circuit in circuits
+		 do (if (and (gethash (metric-one metric) circuit)
+			     (gethash (metric-two metric) circuit))
+			(return t))
+		 finally (return nil))))
+    (remove-if #'already metrics)))
+
+(defun make-circuit (metric)
+  (let ((circuit (make-hash-table :test 'eq)))
+    (setf (gethash (metric-one metric) circuit) t
+	  (gethash (metric-two metric) circuit) t)
+    circuit))
 
 (defun part-1 ()
-  (multiple-value-bind (distances table) (make-distances (load-data "08"))
-    (flet ((subset-of (distances)
-	     (loop with new-table = (make-hash-table)
-		   for distance in distances
-		   do (setf (gethash distance new-table) (gethash distance table))
-		   finally (return new-table))))
-      (let* ((num 1000)
-	     (shortest-n (subseq distances 0 num))
-	     (circuits (make-circuits shortest-n (subset-of shortest-n))))
-      (* (hash-table-count (first circuits))
-	 (hash-table-count (second circuits))
-	 (hash-table-count (third circuits)))))))
+  (let* ((metrics (make-metrics (load-data "08")))
+	 (subset (subseq metrics 0 1000))
+	 (circuits nil))
+    (loop while subset
+	  do (let ((new-circuit (make-circuit (first subset))))
+	       (push new-circuit circuits)
+	       (setf subset (rest subset))
+	       (setf circuits (merge-circuits new-circuit circuits))
+	       (setf subset (remove-already-in subset circuits))))
+    (let ((sorted (sort circuits #'> :key #'hash-table-count)))
+      (* (hash-table-count (first sorted))
+	 (hash-table-count (second sorted))
+	 (hash-table-count (third sorted))))))
 
-#|(defun part-2 ()
-  (multiple-value-bind (distances table) (make-distances (load-data "08a"))
-    (join-others (circuit)
-	       (loop with changed = t
-		     while changed
-		     do (loop for distance being the hash-keys in table
-			      do (destructuring-bind (one . two) (gethash distance table)
-				   (when (or (gethash one circuit)
-					     (gethash two circuit))
-	
-    (loop for distance in distances
-	  do (format t "~A~%" (gethash distance table)))))
-
-|#
+(defun part-2 ()
+  (let* ((metrics (make-metrics (load-data "08")))
+	 (circuits nil)
+	 (index 0)
+	 (last nil))
+    (loop for metric in metrics
+	  do (multiple-value-bind (how circuit) (how-p metric circuits)
+	       (format t "~A ~%" (incf index))
+	       (case how
+		 (:noaction nil)
+		 (:new (push (make-circuit metric) circuits))
+		 (:join
+		  (setf last metric)
+		  (setf (gethash (metric-one metric) circuit) t
+			(gethash (metric-two metric) circuit) t)
+		  (setf circuits (merge-circuits circuit circuits))))))
+    (* (first (metric-one last)) (first (metric-two last)))))
